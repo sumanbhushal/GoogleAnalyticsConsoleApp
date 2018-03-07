32f41e2ed66215bd9099687e7f1cf463720a540c
sumanbhushal <sumanbhushal@gmail.com>
2018-03-07 13:57:13 +0100

    Defining Dimensiona and Matrices with date ranges Bulding reportobject Printing response and saving it to the file

--- GoogleAnalyticsConnector/Program.cs
+++ GoogleAnalyticsConnector/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using System.IO;
+using System.Data;
 using Google.Apis.AnalyticsReporting.v4;
 using Google.Apis.AnalyticsReporting.v4.Data;
 using Google.Apis.Auth.OAuth2;
@@ -31,9 +32,92 @@ namespace GoogleAnalyticsConnector
                 ApplicationName = config.APPLICATION_NAME,
             });
 
+            // Create the DataRange Object
+            DateRange dataRange = new DateRange() { StartDate = "2018-01-01", EndDate = "2018-03-07" };
+
+            //Create the Metrics Object "ga:pageviews,ga:entrances"
+            Metric sessions = new Metric { Expression = "ga:sessions", Alias = "Session" };
+
+            //Crate the Dimensions Object. "ga:date,ga:pagePath,ga:landingPagePath,ga:source"
+            Dimension browser = new Dimension { Name = "ga:browser" };
+
+            //Create the ReportRequest Object
+            ReportRequest reportRequest = new ReportRequest
+            {
+                ViewId = config.VIEW_ID,
+                DateRanges = new List<DateRange>() { dataRange },
+                Dimensions = new List<Dimension>() { browser },
+                Metrics = new List<Metric>() { sessions }
+            };
+
+            List<ReportRequest> requests = new List<ReportRequest>();
+            requests.Add(reportRequest);
+
+            //Create the GetReportsRequest object
+            GetReportsRequest getReport = new GetReportsRequest() { ReportRequests = requests };
+
+            //Call the batchGet Method
+            GetReportsResponse response = service.Reports.BatchGet(getReport).Execute();
+
+            printResults(response.Reports);
+            //writeResultsToFile();
+
         }
 
-        static  UserCredential GetCredential()
+        private static void writeResultsToFile()
+        {
+            /// <summary>
+            /// Method for writing the results to external file at given folder destination, 
+            /// given view_id and format type
+            /// </summary>
+            using (StreamWriter writer = new StreamWriter(config.DESTINATION_FOLDER_PATH + "ga_" + config.VIEW_ID + config.DESTINATION_FILE_TYPE))
+            {
+                writer.WriteLine("This is test file");
+            }
+        }
+
+        private static void printResults(IList<Report> reports)
+        {
+            foreach (Report report in reports)
+            {
+                ColumnHeader header = report.ColumnHeader;
+                List<string> dimensionHeaders = (List<string>)header.Dimensions;
+
+                List<MetricHeaderEntry> metricHeaders = (List<MetricHeaderEntry>)header.MetricHeader.MetricHeaderEntries;
+                List<ReportRow> rows = (List<ReportRow>)report.Data.Rows;
+
+                if (report.Data.Rows == null)
+                {
+                    Console.WriteLine("No Data Available in Google Analytics");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    foreach (ReportRow row in rows)
+                    {
+                        List<string> dimensions = (List<string>)row.Dimensions;
+                        List<DateRangeValues> metrics = (List<DateRangeValues>)row.Metrics;
+
+                        for (int i = 0; i < dimensionHeaders.Count() && i < dimensions.Count(); i++)
+                        {
+                            Console.WriteLine(dimensionHeaders[i] + ": " + dimensions[i]);
+                        }
+
+                        for (int j = 0; j < metrics.Count(); j++)
+                        {
+                            Console.WriteLine("Data Range (" + j + "): ");
+                            DateRangeValues values = metrics[j];
+                            for (int k = 0; k < values.Values.Count() && k < metricHeaders.Count(); k++)
+                            {
+                                Console.WriteLine(metricHeaders[k].Name + ": " + values.Values[k]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static UserCredential GetCredential()
         {
             /// <summary>
             /// Reading Client Secret Credintials, and getting permission to access
@@ -43,7 +127,7 @@ namespace GoogleAnalyticsConnector
             UserCredential credential;
 
             using (var stream =
-                new FileStream(config.APPLICATION_NAME, FileMode.Open, FileAccess.Read))
+                new FileStream(config.CLIENT_SECRET_JSON_RESOURCE_PATH, FileMode.Open, FileAccess.Read))
             {
                 string credPath = System.Environment.GetFolderPath(
                     System.Environment.SpecialFolder.Personal);
--- GoogleAnalyticsConnector/config.cs
+++ GoogleAnalyticsConnector/config.cs
@@ -10,5 +10,8 @@ namespace GoogleAnalyticsConnector
     {
         public static string CLIENT_SECRET_JSON_RESOURCE_PATH = "client_secret.json";
         public static string APPLICATION_NAME = "GoogleAnalyticsApiConsole";
+        public static string VIEW_ID = "XXXX";
+        public static string DESTINATION_FOLDER_PATH = @"C:\Users\XXXX";
+        public static string DESTINATION_FILE_TYPE = ".txt";
     }
 }
